fe1ce6faf253331489e8695477f5421f62bac711
smokeB0x <32621767+smokeB0x@users.noreply.github.com>
2024-05-10 09:54:53 +0200

    Update ei-converter.py

--- ei-converter.py
+++ ei-converter.py
@@ -9,6 +9,7 @@ import sys
 import base64
 import datetime
 
+#checks if there is an argument other than the script (the ei-value) and provides instructions of it does not. also adds the argument (ei-value) to the varable value
 if len(sys.argv) > 1:
     value = sys.argv[1]  
 else:
@@ -18,6 +19,7 @@ else:
 
 print("Inputed value: ", value)
 
+#python base64 decoding needs that the string is dividable by 4. If not this sections adds "=" as padding until it does.
 num_extra_bytes = (len(value) % 4) 
 if (num_extra_bytes != 0):
     padlength = 4 - num_extra_bytes 
@@ -27,12 +29,15 @@ else:
 
 print("Padded base64 string = " + padstring)
 
+#decodes the url-safe base64 string into bytes
 decoded = base64.urlsafe_b64decode(padstring)
 
+#Takes the four first bytes in the value and converts them from little endian into decimal
 timestamp = decoded[0] + decoded[1]*256 + decoded[2]*(256**2) + decoded[3]*(256**3)
 
 print("Timestamp: ", timestamp)
 
+#Converts the timestamp (UNIX epoch) into a human readable date and time
 datetimestr = datetime.datetime.utcfromtimestamp(timestamp).strftime('%d-%m-%Y T%H:%M:%S UTC')
 
 print("Date and time: ", datetimestr)
